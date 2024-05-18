d0d4d47af341c686c66795684772cb3e9515a34d
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-18 20:19:00 +0700

    Lab9 DataTypes&Variable: ระบุค่า

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+let a = 1;              //1
+console.log(`a: ${a}`)
+
+let b = 2;              //2
+console.log(`b: ${b}`)
+
+let c = a++;            //1
+console.log(`c: ${c}`)
+
+let d = ++c;            //2
+console.log(`d: ${d}`)
+
+let e = ++d + d++ + d;  //10
+console.log(`e: ${e}`)
